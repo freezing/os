ca31631a8729cb4e289f1e4fdba801fec53c595b
Nikola <nikolavla@gmail.com>
2019-04-06 21:39:08 +0100

    Add more complex logic to test debugger

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -4,15 +4,29 @@ void dummy_test_entrypoint() {
 }
 
 const char COLOR = 0x0f;
+const char ANOTHER_COLOR = 0xf0;
 
-void main() {
+void draw(const char color) {
   char* video_memory = (char*) 0xb8000;
   char* s = "From C code changed video memory text.";
   char* c = s;
   while (*c != '.') {
     *video_memory = *c;
-    video_memory[1] = COLOR;
+    video_memory[1] = color;
     video_memory += 2;
     c++;
+  }
+}
+
+void main() {
+  int color = COLOR;
+  while (1) {
+    draw(color);
+    if (color == COLOR) {
+      color = ANOTHER_COLOR;
+    }
+    else {
+      color = COLOR;
+    }
   }
 }
