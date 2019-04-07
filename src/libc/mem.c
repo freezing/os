36542ddcdee685cbc2aaea1ccc9f20e7be5c32d4
Nikola <nikolavla@gmail.com>
2019-04-07 13:39:19 +0100

    Implement kmalloc

--- src/libc/mem.c
+++ src/libc/mem.c
@@ -16,3 +16,22 @@ void memory_set(u8 *dest, u8 val, u32 len) {
   }
 }
 
+// This should be computed at link time, but a hardcoded
+// value is fine for now. Remember that our kernel starts
+// at 0x1000 as defined on the Makefile.
+u32 free_mem_addr = 0x10000;
+
+// Implementation is just a pointer to some free memory that keeps growing.
+u32 kmalloc(u32 size, int align, u32 *phys_addr) {
+  if (align == 1 && (free_mem_addr & 0xFFFFF000)) {
+    free_mem_addr &= 0xFFFFF000;
+    free_mem_addr += 0x1000;
+  }
+
+  // Save also the physical address.
+  if (phys_addr) *phys_addr = free_mem_addr;
+  
+  u32 ret = free_mem_addr;
+  free_mem_addr += size;
+  return ret;
+}
--- src/libc/mem.h
+++ src/libc/mem.h
@@ -9,4 +9,10 @@ void memory_copy(char *source, char *dest, int nbytes);
 // Sets the val to memory address range [dest, dest + len).
 void memory_set(u8 *dest, u8 val, u32 len);
 
+// Returns a pointer to a free memory with the specified number of bytes ('size')
+// as well as the corresponding physical address.
+// If align is true, then the pages are aligned to 4K or 0x1000.
+u32 kmalloc(u32 size, int align, u32 *phys_addr);
+// TODO: implement free.
+
 #endif
