48585ee525843ea0add88e23927d920541fc3e65
Nikola <nikolavla@gmail.com>
2019-04-07 13:24:51 +0100

    Very simple shell implementation

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -1,9 +1,23 @@
 
-void keyboard_input(char* input) {
+void on_keyboard_input_changed(char* input) {
+  if (strcmp(input, "END") == 0) {
+    kprint("Stopping the CPU. Bye!\n");
+    asm volatile("hlt");
+  }
 
+  kprint("You said: ");
+  kprint(input);
+  kprint("\n");
+}
+
+void run_shell() {
+  kprint("Type something, it will go through the kernel\n"
+	"Type END to halt the CPU\n");
+  // TODO: Ideally, shell would register the IRQ listener for keyboard here.
 }
 
 void main() {
   isr_install();
-  irq_install(keyboard_input);
+  irq_install(on_keyboard_input_changed);
+  run_shell();
 }
