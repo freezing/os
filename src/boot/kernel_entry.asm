cd0d92a613f07c6acace187c721babcde1603cc8
Nikola <nikolavla@gmail.com>
2019-04-07 13:53:43 +0100

    Fix no global _start label warning

--- src/boot/kernel_entry.asm
+++ src/boot/kernel_entry.asm
@@ -1,4 +1,10 @@
+global _start
 [bits 32]
-[extern kernel_main] ; Define calling point. Must have the same name as kernel.c 'main' function
-call kernel_main ; Calls the C function. The linker will know where it is placed in memory.
-jmp $
+
+_start:
+  ; Define calling point. Must have the same name as kernel.c 'main' function
+  [extern kernel_main] 
+  ; Calls the C function. The linker will know where it is placed in memory.
+  call kernel_main
+  ; Should never happen. Infinite loop.
+  jmp $
