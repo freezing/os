69f3f854251ef43341ce0df022f78384daa599aa
Nikola <nikolavla@gmail.com>
2019-04-07 13:47:14 +0100

    Add PAGE command to shell to test kmalloc

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -3,10 +3,27 @@
 #include "../libc/string.h"
 #include "../libc/mem.h"
 
+void run_kmalloc_test() {
+  u32 phys_addr;
+  u32 page = kmalloc(1000, 1, &phys_addr);
+  char page_str[16] = "";
+  hex_to_ascii(page, page_str);
+  char phys_str[16] = "";
+  hex_to_ascii(phys_addr, phys_str);
+  kprint("Page: ");
+  kprint(page_str);
+  kprint(", physical address: ");
+  kprint(phys_str);
+  kprint("\n");
+}
+
 void on_keyboard_input_changed(char* input) {
   if (strcmp(input, "END") == 0) {
     kprint("Stopping the CPU. Bye!\n");
     asm volatile("hlt");
+  } else if (strcmp(input, "PAGE") == 0) {
+    // Code to test kmalloc, the rest is unchanged.
+    run_kmalloc_test();
   }
 
   kprint("You said: ");
@@ -16,7 +33,7 @@ void on_keyboard_input_changed(char* input) {
 
 void run_shell() {
   kprint("Type something, it will go through the kernel\n"
-	"Type END to halt the CPU\n");
+        "Type END to halt the CPU or PAGE to request a kmalloc()\n> ");
   // TODO: Ideally, shell would register the IRQ listener for keyboard here.
 }
 
